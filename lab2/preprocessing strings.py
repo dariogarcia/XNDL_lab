a4dead79b5b128cb68e98475b4c480c19d2b9b84
dariogarcia <dario.garcia@bsc.es>
2023-03-11 00:52:47 +0100

    removes unused import

--- lab2/preprocessing strings.py
+++ lab2/preprocessing strings.py
@@ -1,4 +1,3 @@
-from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
